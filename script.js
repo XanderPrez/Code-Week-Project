565507e31f2707aa083152e9f666b85f4d8eb03e
Alessandro <Alexanderg90@hotmail.it>
2022-04-04 19:38:18 +0200

    aggiunta-funzione-select

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-let apiKey = '1d00d4c9d5554bbb1fc814776caff71d';
-let daUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+
+/* SELETTORE */
+const q = (selector) => document.querySelector(selector);
+
+/* FUNZIONE SELECT CITY */
+const selectedEl = q('.citySelector'); // seleziono il select in html
+
+let apiKey = '1d00d4c9d5554bbb1fc814776caff71d'; // la mia chiave API
+
+selectedEl.addEventListener('change', () => { //con questa funzione, al cambio del valore nel selettore html, modifico l'url dell'API e aggiungo la key
+    let city = selectedEl.value;
+    let daUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+apiKey;
+    // console.log(daUrl)
+});
